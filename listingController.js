33b363a4a4f3414953f668bfef7f587ad41b1475
Fernando Quintero <ferdanielq@outlook.com>
2018-09-10 13:28:38 -0400

    Fixed controller, added table to view

--- listingController.js
+++ listingController.js
@@ -2,17 +2,20 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.form = undefined;
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
     $scope.addListing = function(code, name, latitude, longitude, address) {
-      $scope.listings.entries.push({ code, name, coordinates: {latitude, longitude}, address });
+      $scope.listings.push({ code, name, coordinates: {latitude, longitude}, address });
     };
     $scope.deleteListing = function(index) {
-      $scope.listings.entries.splice(index, 1);
+      $scope.listings.splice(index, 1);
     };
-    $scope.showDetails = function(index) {};
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
