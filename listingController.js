68198dd6ad8ecf2a76474eb8d20cc9548fe085d5
Fernando Quintero <ferdanielq@outlook.com>
2018-09-10 12:51:49 -0400

    refined controller

--- listingController.js
+++ listingController.js
@@ -7,8 +7,8 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function(code, name, coordinates, address) {
-      $scope.listings.entries.push({ code, name, coordinates, address });
+    $scope.addListing = function(code, name, latitude, longitude, address) {
+      $scope.listings.entries.push({ code, name, coordinates: {latitude, longitude}, address });
     };
     $scope.deleteListing = function(index) {
       $scope.listings.entries.splice(index, 1);
