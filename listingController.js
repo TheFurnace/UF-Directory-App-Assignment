3fc835ed8cb45869d1a3e83ff788eed2124b4b9f
Fernando Quintero <ferdanielq@outlook.com>
2018-09-10 14:23:29 -0400

    Finished all but show details

--- listingController.js
+++ listingController.js
@@ -2,7 +2,7 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-    $scope.form = undefined;
+    $scope.form = $scope.emptyForm;
 
     /* 
       Implement these functions in the controller to make your application function 
@@ -10,12 +10,20 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
      */
     $scope.addListing = function(code, name, latitude, longitude, address) {
       $scope.listings.push({ code, name, coordinates: {latitude, longitude}, address });
+      $scope.listings.sort((a,b) => { return a.code > b.code });
     };
-    $scope.deleteListing = function(index) {
-      $scope.listings.splice(index, 1);
+    $scope.deleteListing = function(listing) {
+      $scope.listings.forEach((entry, index) => {
+        if(entry.code === listing.code) {
+          $scope.listings.splice(index, 1);
+        }
+      });
     };
     $scope.showDetails = function(index) {
       $scope.detailedInfo = $scope.listings[index];
     };
+    $scope.clear = function() {
+      $scope.form = $scope.emptyForm; 
+    }
   }
 ]);
